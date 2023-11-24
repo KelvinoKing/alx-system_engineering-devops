c025a628a957870ecbe45603c3b121fa4534c385
KelvinoKing <kelvinoking@outlook.com>
2023-11-24 18:10:50 +0300

    Hot fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file in /tmp
+# manifest to create file in tmp directory
 file { '/tmp/school':
-  ensure  => present,
-  path    => '/tmp/school'
+  ensure  => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data'
+  group   => 'www-data',
   content => 'I love Puppet',
 }
