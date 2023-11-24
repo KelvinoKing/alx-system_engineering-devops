9278b18c936f071518aca06b8cf61a8c01941166
KelvinoKing <kelvinoking@outlook.com>
2023-11-24 18:58:01 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
 # Install flask
-
-package { 'python3-pip':
-ensure => installed,
-}
-
-
 package { 'Flask':
-  ensure  => '2.1.0',
-  require => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
