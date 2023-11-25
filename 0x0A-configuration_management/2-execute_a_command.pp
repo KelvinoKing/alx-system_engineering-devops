3874c0d7d8b9a967281996fcefd0f760d1121652
KelvinoKing <kelvinoking@outlook.com>
2023-11-25 09:57:37 +0300

    Kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 #Using Puppet we create a manifest that kills a process named killmenow
 exec {'pkill killmenow':
-  command  => 'usr/bin/pkill killmenow',
+  command     => 'usr/bin/pkill -9 killmenow',
+  refreshonly => true,
 }
