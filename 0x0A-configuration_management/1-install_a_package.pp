2d56cdc01bf5831da94d21138615a3fcd32d98f0
KelvinoKing <kelvinoking@outlook.com>
2023-11-25 09:54:33 +0300

    Intall flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # Install Flask 2.1.0
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/bin/flask',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
