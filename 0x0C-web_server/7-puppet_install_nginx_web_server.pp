40597a441cc137688d17e435e8d817f5990ae9ad
KelvinoKing <kelvinoking@outlook.com>
2023-11-28 06:32:13 +0000

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# File: nginx_config.pp
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80;',
+  line   => 'rewrite ^/redirect_me https://kelvino.com permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
