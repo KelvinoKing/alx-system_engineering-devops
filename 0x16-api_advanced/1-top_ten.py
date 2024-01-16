5fa13249a555a0aa281530b72da62566a2f89b0f
KelvinoKing <kelvinoking@outlook.com>
2024-01-16 11:08:43 +0000

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
-"""import requests module
+"""
+1-top_ten
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints 10 post's titles
-    """
-
+    # defining the limit
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Kelvino'}
+
+    # setting user-agent
+    headers = {'User-Agent': 'My Reddit API Client'}
 
     try:
+        # Making request
         response = requests.get(url, headers=headers)
         response.raise_for_status()
 
         data = response.json()
 
+        # check if data containing posts
         if 'data' in data and 'children' in data['data']:
+            # Extract and print the titles of the first 10 posts
             for post in data['data']['children']:
                 print(post['data']['title'])
         else:
             print("None")
     except requests.exceptions.RequestException:
+
         print("None")
     except KeyError:
+
         print("None")
+
+
+if __name__ == '__main__':
+
+    subreddit = "cybersecurity"
+    top_ten(subreddit)
