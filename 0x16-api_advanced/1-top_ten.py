af14721a33c7d5a26e3c445e149355251bce3866
KelvinoKing <kelvinoking@outlook.com>
2024-01-16 11:11:57 +0000

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,22 +7,17 @@ import requests
 
 
 def top_ten(subreddit):
-    # defining the limit
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
-    # setting user-agent
     headers = {'User-Agent': 'My Reddit API Client'}
 
     try:
-        # Making request
         response = requests.get(url, headers=headers)
         response.raise_for_status()
 
         data = response.json()
 
-        # check if data containing posts
         if 'data' in data and 'children' in data['data']:
-            # Extract and print the titles of the first 10 posts
             for post in data['data']['children']:
                 print(post['data']['title'])
         else:
