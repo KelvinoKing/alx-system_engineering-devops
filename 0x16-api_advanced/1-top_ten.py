352dfc553d3051322133cbb733081a601024670b
KelvinoKing <kelvinoking@outlook.com>
2024-01-16 11:14:04 +0000

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,8 @@ import requests
 
 
 def top_ten(subreddit):
+    """prints out 10 post titles
+    """
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
     headers = {'User-Agent': 'My Reddit API Client'}
