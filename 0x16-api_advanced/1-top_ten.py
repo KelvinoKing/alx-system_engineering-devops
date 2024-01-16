5d3d466a85047ce7a5e0aa755b58757c066ebe4e
KelvinoKing <kelvinoking@outlook.com>
2024-01-16 11:07:05 +0000

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     """
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'My Reddit API Client'}
+    headers = {'User-Agent': 'Kelvino'}
 
     try:
         response = requests.get(url, headers=headers)
